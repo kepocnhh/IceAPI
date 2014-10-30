4552b7359a00d65b8e7f628894af6c46f748fd87
toha <toha@localhost>
2014-10-30 17:09:47 +0400

    Нужно попробовать другую библиотеку для XLS

--- src/api/CreateXLS.java
+++ src/api/CreateXLS.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -24,8 +25,6 @@ public class CreateXLS
         String pre = "";
         String pro = "";
         String mulct = "";
-            System.out.println("newitog.get_DC() "+newitog.get_DC());
-            System.out.println("newitog.get_DC().length "+newitog.get_DC().length);
         for(int i=0;i<newitog.get_DC().length;i++)
         {
             if(newitog.get_DC()[i].getTypeEvent()==DataCass.TypeEvent.inkasator)
@@ -59,47 +58,66 @@ public class CreateXLS
         {
             mulct+=" "+newitog.get_mulct()[i];
         }
+            System.out.println("get_mulct");
         mulct+="\t";
         for(int i=0;i<newitog.get_mulct().length;i++)
         {
             mulct+=" "+newitog.get_mulct_str()[i];
         }
-        String[] cell=(
-                newitog.date_open+"\t"+
-                newitog.amount_k+"\t"+
-                newuser.price_k+"\t"+
-                (newitog.amount_k*newuser.price_k)+"\t"+
-                newitog.amount_s+"\t"+
-                newuser.price_s+"\t"+
-                (newitog.amount_s*newuser.price_s)+"\t"+
-                newitog.amount_t+"\t"+
-                newuser.price_t+"\t"+
-                (newitog.amount_t*newuser.price_t)+"\t"+
-                ((newitog.amount_k*newuser.price_k)+(newitog.amount_s*newuser.price_s)+(newitog.amount_t*newuser.price_t))+"\t"+
-                newuser.GetSurname()+"\t"+
-                newuser.bonus+"\t"+
-                (newitog.amount_k*newuser.bonus)+"\t"+
-                newuser.weight_k+"\t"+
-                (newitog.amount_k*newuser.weight_k)+"\t"+
-                newuser.weight_s+"\t"+
-                (newitog.amount_s*newuser.weight_s)+"\t"+
-                newuser.weight_t+"\t"+
-                (newitog.amount_t*newuser.weight_t)+"\t"+
-                ((newitog.amount_k*newuser.weight_k)+(newitog.amount_s*newuser.weight_s)+(newitog.amount_t*newuser.weight_t))+"\t"+
-                newitog.date_open.getHours() + ":" + CreatePDF.minutes(""+newitog.date_open.getMinutes())+"\t"+
-                newitog.date_close.getHours() + ":" + CreatePDF.minutes(""+newitog.date_close.getMinutes())+"\t"+
-                (double)((newitog.date_close.getHours()-newitog.date_open.getHours()) * 60 +
-                    (newitog.date_close.getMinutes()-newitog.date_open.getMinutes())) / 60+"\t"+
-                newuser.salary+"\t"+
-                newitog.salary+"\t"+
-                (newitog.salary+newitog.amount_k*newuser.bonus)+"\t"+
-                ink+"\t"+
-                pre+"\t"+
-                pro+"\t"+
-                mulct+"\t"+
-                newitog.day_otw+"\t"+
-                ((newitog.salary+newitog.amount_k*newuser.bonus)-newitog.get_summ_mulct())
-                ).split("\t");
+            System.out.println("get_mulct_str");
+            String str = "";
+            str +=newitog.date_open+"\t";
+            str +=newitog.amount_k+"\t";
+            str +=newuser.price_k+"\t";
+               str += (newitog.amount_k*newuser.price_k)+"\t";
+                str +=newitog.amount_s+"\t";
+                str +=newuser.price_s+"\t";
+                str +=(newitog.amount_s*newuser.price_s)+"\t";
+                str +=newitog.amount_t+"\t";
+                str +=newuser.price_t+"\t";
+                System.out.println("price");
+                str +=(newitog.amount_t*newuser.price_t)+"\t";
+                str +=((newitog.amount_k*newuser.price_k)+(newitog.amount_s*newuser.price_s)+(newitog.amount_t*newuser.price_t))+"\t";
+                str +=newuser.GetSurname()+"\t";
+                System.out.println("Surname = "+newuser.GetSurname());
+                str +=newuser.bonus+"\t";
+                str +=(newitog.amount_k*newuser.bonus)+"\t";
+                str +=newuser.weight_k+"\t";
+                str +=(newitog.amount_k*newuser.weight_k)+"\t";
+                str +=newuser.weight_s+"\t";
+                str +=(newitog.amount_s*newuser.weight_s)+"\t";
+                str +=newuser.weight_t+"\t";
+                str +=(newitog.amount_t*newuser.weight_t)+"\t";
+                str +=((newitog.amount_k*newuser.weight_k)+(newitog.amount_s*newuser.weight_s)+(newitog.amount_t*newuser.weight_t))+"\t";
+            System.out.println("amount");
+            String s;
+            s = ""+newitog.date_open.getMinutes();
+        if (s.length() == 1)
+        {
+            s = "0" + s;
+        }
+                str +=newitog.date_open.getHours() + ":" + s+"\t";
+            s = ""+newitog.date_close.getMinutes();
+        if (s.length() == 1)
+        {
+            s = "0" + s;
+        }
+                str +=newitog.date_close.getHours() + ":" + s+"\t";
+            System.out.println("Hours");
+                str +=(double)((newitog.date_close.getHours()-newitog.date_open.getHours()) * 60 +(newitog.date_close.getMinutes()-newitog.date_open.getMinutes())) / 60+"\t";
+                str +=newuser.salary+"\t";
+                str +=newitog.salary+"\t";
+                str +=(newitog.salary+newitog.amount_k*newuser.bonus)+"\t";
+            System.out.println("dc");
+                str +=ink+"\t";
+                str +=pre+"\t";
+                str +=pro+"\t";
+                str +=mulct+"\t";
+                str +=newitog.day_otw+"\t";
+                str +=((newitog.salary+newitog.amount_k*newuser.bonus)-newitog.get_summ_mulct());
+        String[] cell=(str).split("\t");
+            System.out.println("split");
+            r.setHeightInPoints(111);
         for (int i = 0; i < cell.length; i++)
         {
             c = r.createCell(i);
@@ -107,15 +125,8 @@ public class CreateXLS
         }
         return r;
     }
-    static public void _CreateXLS(
-            Date d1, Date d2,
-            String name) throws FileNotFoundException, IOException, ClassNotFoundException
-    {
-        
-    }
-    static public void _CreateXLS(
-            Itog newitog[],
-            String name) throws FileNotFoundException, IOException, ClassNotFoundException
+    static public void _CreateXLS(Itog newitog[],String name,List<user> ul) 
+            throws FileNotFoundException, IOException, ClassNotFoundException
     {
         Workbook wb = new HSSFWorkbook();
         Sheet s = wb.createSheet();
@@ -176,9 +187,24 @@ public class CreateXLS
             c.setCellValue(cell[i]);
         }
             System.out.println("create_row");
+                System.out.println("\n"+newitog[0].user_email);
         for(int i = 0; i < newitog.length; i++)
         {
-            //create_row(newitog[i], API.Find_user(API.accpath, newitog[i].user_email), s.createRow(1));//ВОТ ЗДЕСЬ ИСПРАВИТЬ!!!
+                    user newuser = null;
+            for (user ul1 : ul) 
+            {
+                if (newitog[i].user_email.equals(ul1.GetMail())) 
+                {
+                    newuser = ul1;
+                    break;
+                }
+            }
+            if(newuser == null)
+            {
+                System.out.println("user null\n"+newitog[i].user_email);
+                continue;
+            }
+            create_row(newitog[i], newuser, s.createRow(1));
         }
         FileOutputStream out = new FileOutputStream(name+".xls");
             wb.write(out);
